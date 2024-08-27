5ac4ebed89ceca556732c27c2f8480ef30ba0927
Joachim Øen <joachimoen02@gmail.com>
2024-08-27 18:16:39 +0200

    Endringar

--- Oblig1_h24_lambda_og_strommer/src/oppg1b/Main.java
+++ Oblig1_h24_lambda_og_strommer/src/oppg1b/Main.java
@@ -1,10 +1,24 @@
 package oppg1b;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		beregn regnUt = new beregn();
 		
-
+		int a;
+		int b;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Legg inn det første nummeret");
+			a = scanner.nextInt();
+		System.out.println("Legg inn det andre nummeret");
+			b = scanner.nextInt();
+			
+		System.out.println("Kva skal eg printe ut?");	
+			scanner.close();
 	}
 
 }
--- Oblig1_h24_lambda_og_strommer/src/oppg1b/beregn.java
+++ Oblig1_h24_lambda_og_strommer/src/oppg1b/beregn.java
@@ -2,18 +2,69 @@ package oppg1b;
 
 
 
-public class beregn {
+public class beregn implements BeregnInterface {
 	public static int beregn(int a, int b, char teikn) {
 
-		//if(teikn != '+' || teikn != '-' || teikn != '*' || teikn != '/') {
-			//System.out.print("Dette matteteiknet er ikkje støtta!");
-		//}
-//Treng eg dette her, eller dette på denne måten?		
+		if(teikn != '+' || teikn != '-' || teikn != '*' || teikn != '/') {
+			System.out.print("Dette matteteiknet er ikkje støtta!");
+		}
+	
 		if( b == 0 && teikn == '/') {
 			System.out.print("Kan ikkje dele på 0!");
 		}
 		
-		return a + teikn + b;
+		switch(teikn) {
+		case '+': System.out.println(a + b);
+		case '-': System.out.println(a - b);
+		case '*': System.out.println(a * b);
+		case '/': System.out.println(a / b);
+		}
+//Korleis i svarte skal eg finne ut av ein måte å bruke
+//Math.max her?
 		
-	}
+		return teikn;
+			}
+
+//	@Override
+//	public int adderTal(int a, int b) {
+//		return a + b;
+//	}
+//
+//	@Override
+//	public int subtraherTal(int a, int b) {
+//		
+//		return a - b;
+//	}
+//
+//	@Override
+//	public int multipliserTal(int a, int b) {
+//		
+//		return a * b;
+//	}
+//
+//	@Override
+//	public int dividerTal(int a, int b) {
+//		
+//		return a / b;
+//	}
+//
+//	@Override
+//	public int storstTal(int a, int b) {
+//		
+//		return Math.max(a, b) ;
+//	}
+//
+//	@Override
+//	public int minstTal(int a, int b) {
+//		return Math.min(a, b);
+//	}
+//
+//	@Override
+//	public int avstandMellomTal(int a, int b) {
+//		int sum = a + b; 
+//		if(sum < 0) {
+//			return Math.abs(sum);
+//		}
+//		return sum;
+//	}
 }
