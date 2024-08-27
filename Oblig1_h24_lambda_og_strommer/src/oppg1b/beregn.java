5eb07bd6c411e70f078cac377b08d0ddff5bf6b2
Joachim Øen <joachimoen02@gmail.com>
2024-08-27 16:45:00 +0200

    Endringar

--- Oblig1_h24_lambda_og_strommer/src/oppg1b/beregn.java
+++ Oblig1_h24_lambda_og_strommer/src/oppg1b/beregn.java
@@ -1,5 +1,19 @@
 package oppg1b;
 
-public class beregn {
 
+
+public class beregn {
+	public static int beregn(int a, int b, char teikn) {
+
+		//if(teikn != '+' || teikn != '-' || teikn != '*' || teikn != '/') {
+			//System.out.print("Dette matteteiknet er ikkje støtta!");
+		//}
+//Treng eg dette her, eller dette på denne måten?		
+		if( b == 0 && teikn == '/') {
+			System.out.print("Kan ikkje dele på 0!");
+		}
+		
+		return a + teikn + b;
+		
+	}
 }
