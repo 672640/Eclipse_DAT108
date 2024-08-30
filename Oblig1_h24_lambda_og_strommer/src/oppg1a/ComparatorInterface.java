dc5b116c363e4913735bae1a9e6caaecad1f2dc7
Joachim Øen <142489531+672640@users.noreply.github.com>
2024-08-30 17:24:51 +0200

    Endringar

--- Oblig1_h24_lambda_og_strommer/src/oppg1a/ComparatorInterface.java
+++
@@ -1,9 +0,0 @@
-package oppg1a;
-
-import java.util.*;
-
-@FunctionalInterface
-
-public interface ComparatorInterface {
-	public int compare(List<String> a, List<String> b);
-}
--- Oblig1_h24_lambda_og_strommer/src/oppg1a/Main.java
+++ Oblig1_h24_lambda_og_strommer/src/oppg1a/Main.java
@@ -1,38 +1,20 @@
 package oppg1a;
 
 import java.util.*;
-
-
+@FunctionalInterface
+interface nummera {
+	int sorter(int a);
+}
 
 public class Main{
-
-	
 	
 	public static void main(String[] args) {
 
 		List<String> listen = Arrays.asList("10", "1", "20", "110", "21", "12");
 		
-		List<List<String>> innpakkaListe = new ArrayList<>();
-		for(String str: listen) {
-			innpakkaListe.add(Collections.singletonList(str));
+		for(String i: listen) {
+			Collections.
 		}
 		
-		ComparatorInterface comparator = (List<String> a, List<String> b) ->
-		Integer.parseInt(a.get(0)).compareTo(Integer.parseInt(b.get(0)));
-
-		innpakkaListe.sort(comparator::compare);
-		
-		List<String> sortertListe = new ArrayList<>();
-		for(List<String> wrapped : innpakkaListe) {
-			sortertListe.add(innpakkaListe.get(0));
-		}
-		
-		System.out.println(sortertListe);
 	}
-
-
-	
-
-
-
 }
