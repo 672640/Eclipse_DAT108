d7371be6c32c649be8bbebd7bb3e343aee3939db
Joachim Øen <joachimoen02@gmail.com>
2024-09-30 17:05:16 +0200

    Endringar

--- JSDemo/src/main/java/no/hvl/dat108/js/JsDemoApplication.java
+++ JSDemo/src/main/java/no/hvl/dat108/js/JsDemoApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package no.hvl.dat108.js;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JsTestApplication {
+public class JsDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JsTestApplication.class, args);
+		SpringApplication.run(JsDemoApplication.class, args);
 	}
 
 }
--- JSDemo/src/test/java/no/hvl/dat108/js/JsDemoApplicationTests.java
+++ JSDemo/src/test/java/no/hvl/dat108/js/JsDemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package no.hvl.dat108.js;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JsTestApplicationTests {
+class JsDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
