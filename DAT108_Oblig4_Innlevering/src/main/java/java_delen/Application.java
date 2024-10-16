13624dd2bc99e2973fd4570558cc978caeda6b02
Joachim Øen <142489531+672640@users.noreply.github.com>
2024-10-16 14:30:12 +0200

    AHHH

---
+++ DAT108_Oblig4_Innlevering/src/main/java/java_delen/Application.java
@@ -0,0 +1,18 @@
+package java_delen;
+/*
+ * Det er denne som kjøres når vi gjør Run As | Java Application.
+ * 
+ * Den starter opp en web-tjener på port 8080 der mappingene vi
+ * har laget i controllerne er tilgjengelige og virker.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ DAT108_Oblig4_Innlevering/src/test/java/test_delen/HalloApplicationTests.java
@@ -0,0 +1,11 @@
+package test_delen;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
